30bb6ad948d5a7762bd512131f1950936e2f9d28
Evans Sese <sese_evans@yahoo.com>
2024-01-11 22:47:18 +0300

    [Feature] Task 5

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """Function to add list items"""
 
+from typing import List
 
-def sum_list(input_list: list[float]) -> float:
+
+def sum_list(input_list: List[float]) -> float:
     """Function to add list items"""
     return sum(input_list)
 
