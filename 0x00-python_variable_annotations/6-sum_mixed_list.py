b5b32cd21af2dcaacad9a9c5a6dfb97afe0103ec
Evans Sese <sese_evans@yahoo.com>
2024-01-11 22:53:15 +0300

    [Feature] Task 6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Function to add list items"""
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_list: List[Union[int,float]]) -> float:
+    """Function to add list items"""
+    return sum(mxd_list)
+
+
+if __name__ == "__main__":
+    sum_mixed_list([0.0, 0])
