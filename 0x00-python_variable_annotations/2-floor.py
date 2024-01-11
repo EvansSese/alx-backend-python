2f74c540a802b1f233d30be1df9d04460507a8ba
Evans Sese <support@ultimate-tracking.co.ke>
2024-01-11 16:51:03 +0300

    [Feature] Task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Function to return floor of a value"""
+
+
+def floor(n: float) -> int:
+    """Function to return floor of a value"""
+    return int(n)
+
+
+if __name__ == "__main__":
+    floor(0.0)
