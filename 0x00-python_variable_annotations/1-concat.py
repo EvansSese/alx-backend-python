10b14a80b0350a3d9c4a2d598a148b3eaf74eb3d
Evans Sese <support@ultimate-tracking.co.ke>
2024-01-11 16:26:59 +0300

    [Feature] Task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Function to concat 2 strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Function to concat 2 strings"""
+    return str1 + str2
+
+
+if __name__ == "__main__":
+    concat('a', 'b')
