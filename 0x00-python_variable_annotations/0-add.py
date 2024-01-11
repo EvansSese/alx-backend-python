dca06eb7c59fca7a5f5f01f3ae348e20494b32a7
Evans Sese <support@ultimate-tracking.co.ke>
2024-01-11 12:46:52 +0300

    [Feature] Task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Function to add 2 floats and return a float"""
+
+
+def add(a: float, b: float) -> float:
+    """Function to add 2 numbers"""
+    return a + b
+
+
+if __name__ == "__main__":
+    add(0, 0)
