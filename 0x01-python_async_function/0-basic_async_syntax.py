caccd729b23726d877385672020db17a69908baa
Evans Sese <sese_evans@yahoo.com>
2024-01-15 14:37:23 +0300

    [Feature] Task 0

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,12 +5,8 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: float = 10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """Async wait_random function/coroutine"""
     rand_wait = random.uniform(0, max_delay)
     await asyncio.sleep(rand_wait)
     return rand_wait
-
-
-if __name__ == "__main__":
-    wait_random()
