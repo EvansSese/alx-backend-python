da02f9685129393db4d7f55126bf9c1877066754
Evans Sese <sese_evans@yahoo.com>
2024-02-05 15:15:13 +0300

    [Feature] Task 7

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -52,6 +52,16 @@ class TestGithubOrgClient(unittest.TestCase):
         expected_result = ["repo1", "repo2", "repo3"]
         self.assertEqual(result, expected_result)
 
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected_result):
+        """Create an instance of GithubOrgClient"""
+        github_org_client = GithubOrgClient("testorg")
+        result = github_org_client.has_license(repo, license_key)
+        self.assertEqual(result, expected_result)
+
 
 if __name__ == '__main__':
     unittest.main()
