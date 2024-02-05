741aa5e18c78c44ceb8f12a5fb29f08f590710b1
Evans Sese <sese_evans@yahoo.com>
2024-02-05 15:13:12 +0300

    [Feature] Task 6

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -35,6 +35,23 @@ class TestGithubOrgClient(unittest.TestCase):
             result = github_org_client._public_repos_url
             self.assertEqual(result, known_payload["repos_url"])
 
+    @patch('client.get_json')
+    @patch('client.GithubOrgClient._public_repos_url',
+           new_callable=PropertyMock,
+           return_value="https://api.github.com/orgs/testorg/repos")
+    def test_public_repos(self, mock_public_repos_url, mock_get_json):
+        """Create a known payload for mocking the get_json method"""
+        known_payload = [{"name": "repo1"}, {"name": "repo2"},
+                         {"name": "repo3"}]
+        mock_get_json.return_value = known_payload
+        github_org_client = GithubOrgClient("testorg")
+        result = github_org_client.public_repos()
+        mock_public_repos_url.assert_called_once()
+        mock_get_json.assert_called_once_with(
+            "https://api.github.com/orgs/testorg/repos")
+        expected_result = ["repo1", "repo2", "repo3"]
+        self.assertEqual(result, expected_result)
+
 
 if __name__ == '__main__':
     unittest.main()
