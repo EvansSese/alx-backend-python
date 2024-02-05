01e18e5b6d2061417dd2224b7bb57e6fa433ac8e
Evans Sese <sese_evans@yahoo.com>
2024-02-05 14:28:11 +0300

    [Feature] Task 3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,7 @@
 import unittest
 from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -45,6 +45,27 @@ class TestGetJson(unittest.TestCase):
             mock_get.assert_called_once_with(test_url)
             """Assert that the result of get_json is equal to test_payload"""
             self.assertEqual(result, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    class TestClass:
+
+        def a_method(self):
+            return 42
+
+        @memoize
+        def a_property(self):
+            return self.a_method()
+
+    @patch.object(TestClass, 'a_method')
+    def test_memoize(self, mock_a_method):
+        """Create an instance of TestClass"""
+        test_instance = self.TestClass()
+        result1 = test_instance.a_property
+        result2 = test_instance.a_property
+        mock_a_method.assert_called_once()
+        self.assertEqual(result1, 42)
+        self.assertEqual(result2, 42)
 
 
 if __name__ == "__main__":
