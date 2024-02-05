c51863d32cf78780a66f06d7d08b9db22cafd604
Evans Sese <sese_evans@yahoo.com>
2024-02-05 15:08:27 +0300

    [Feature] Task 5

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,7 +2,7 @@
 """Test client suite"""
 
 import unittest
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch, MagicMock, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -20,8 +20,20 @@ class TestGithubOrgClient(unittest.TestCase):
         github_org_client = GithubOrgClient(org_name)
         mock_get_json.return_value = MagicMock()
         result = github_org_client.org()
-        mock_get_json.assert_called_once_with(GithubOrgClient.ORG_URL.format(org=org_name))
+        mock_get_json.assert_called_once_with(
+            GithubOrgClient.ORG_URL.format(org=org_name))
         self.assertIsInstance(result, MagicMock)
+
+    def test_public_repos_url(self):
+        """Create a known payload for mocking the org method"""
+        known_payload = {
+            "repos_url": "https://api.github.com/orgs/testorg/repos"}
+        with patch('client.GithubOrgClient.org',
+                   new_callable=PropertyMock,
+                   return_value=known_payload):
+            github_org_client = GithubOrgClient("testorg")
+            result = github_org_client._public_repos_url
+            self.assertEqual(result, known_payload["repos_url"])
 
 
 if __name__ == '__main__':
